b84be296196f038039e225b02b7c97c8db09c26f
Na1x <na1x.1024@gmail.com>
2023-09-28 11:48:46 +0300

    fix: now working drag-n-drop

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const dropFileZone = document.querySelector(".drop-zone");
 const statusText = document.getElementById("uploadForm_Status");
-const inputFiles = document.querySelector("input-files");
+const sizeText = document.getElementById("uploadForm_Size");
+const inputFiles = document.querySelector(".input-files");
 
 let setStatus = (text) => {
     statusText.textContent = text;
@@ -8,29 +9,106 @@ let setStatus = (text) => {
 
 const uploadUrl = "/unicorns";
 
-["dragover", "drop"].forEach((event) => {
+["dragover", "drop"].forEach(function (event) {
     document.addEventListener(event, function (evt) {
-        evt.preventDefault()
-        return false
+        evt.preventDefault();
+        return false;
     })
 })
 
 dropFileZone.addEventListener("dragenter", () => { dropFileZone.classList.add("_active"); });
 dropFileZone.addEventListener("dragleave", () => { dropFileZone.classList.remove("_active"); });
 
+
 dropFileZone.addEventListener("drop", function (event) {
     dropFileZone.classList.remove("_active");
-    const files = event.dataTransfer?.files[0];
-    if (!files) {
-        return
-    }
-    console.log(files);
-    inputFiles.files = event.dataTransfer.files;
-});
+    const files = event.dataTransfer?.files;
 
-inputFiles.addEventListener("change", (event)=>{
-    const files = inputFiles.files?.[0];
-    if(files){
-        console.log("loading...");
+    console.log(event);
+
+    if (!files) {
+        return;
     }
-});
+
+    // if (file.type.startsWith("image/")) {
+        inputFiles.files = event.dataTransfer.files;
+        // processingUploadFile()
+    // } else {
+        // setStatus("Можно загружать только изображения")
+        // return false
+    // }
+})
+
+inputFiles.addEventListener("change", (event) => {
+    const files = inputFiles.files?.[0]
+    if (files) {
+        // console.log(files);
+        // processingUploadFile(files)
+    } else {
+        setStatus("err")
+        return false
+    }
+})
+
+function processingUploadFile(files) {
+    if (files) {
+        const dropZoneData = new FormData()
+        const xhr = new XMLHttpRequest()
+
+        dropZoneData.append("file", files)
+
+        xhr.open("POST", uploadUrl, true)
+
+        xhr.send(dropZoneData)
+
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                setStatus("Всё загружено")
+            } else {
+                setStatus("Oшибка загрузки")
+            }
+            HTMLElement.style.display = "none"
+        }
+    }
+}
+
+function processingDownloadFileWithFetch() {
+    fetch(url, {
+        method: "POST",
+    }).then(async (res) => {
+        const reader = res?.body?.getReader();
+        while (true && reader) {
+            const { value, done } = await reader?.read()
+            console.log("value", value)
+            if (done) break
+            console.log("Received", value)
+        }
+    })
+}
+
+
+
+
+// dropFileZone.addEventListener("drop", function (event) {
+//     dropFileZone.classList.remove("_active");
+//     const files = event.dataTransfer?.files;
+//     console.log(files);
+//     if (!files) {
+//         return
+//     }
+//     console.log(files);
+//     inputFiles.files = event.dataTransfer.files;
+// });
+
+// inputFiles.addEventListener("change", (event)=>{
+//     const files = inputFiles.files?.[0];
+//     if(files){
+//         console.log("loading...");
+//     }
+// });
+
+
+
+
+
+
