1d4417915a4846ba1d92cfd30510bd3ac54383c1
Na1x <na1x.1024@gmail.com>
2023-09-29 17:10:08 +0300

    feat: files come to server

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import multer from 'multer';
+import * as zip from "@zip.js/zip.js";
+import * as fs from "node:fs";
 
 const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 const PORT = 3000;
 
+let zipArray = [];
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.post('/upload', upload.array('files', 100), (req, res, next) => {
-    // res.send(`${req.files.length} files loaded`);
     console.log(`${req.files.length} files loaded`);
+    zipArray = req.files;
+    // zipArray.sort(compare);
+    console.log(zipArray);
+    // console.log(getClearNumbersStr(zipArray[0]));
 })
 
 app.get('/', (req, res) => {
@@ -21,3 +27,27 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`The Server is running on port ${PORT}...`));
+
+async function openZipArray() {
+
+   
+    // const reader = new zip.ZipReader(zipArray[0]);
+    // const entries = await reader.getEntries();
+    // if(entries.length){
+    //     console.log(entries[0]);
+    // }
+}
+
+function compare(a, b) {
+    if (getClearNumbersStr(a) < getClearNumbersStr(b)) {
+        return -1;
+    }
+    if (getClearNumbersStr(a) > getClearNumbersStr(b)) {
+        return 1;
+    }
+    return 0;
+}
+
+function getClearNumbersStr(a){
+    return a.originalname.replace(/[^\d]/g, '0');
+}
