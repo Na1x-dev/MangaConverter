db75574f399ab2224a0432a5ee672ad89f2c590d
Na1x <na1x.1024@gmail.com>
2023-09-30 16:24:30 +0300

    feat: can unzip zip-archives

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import multer from 'multer';
-import * as zip from "@zip.js/zip.js";
 import * as fs from "node:fs";
+import unzipper from 'unzipper';
 
 const upload = multer({ dest: 'uploads/' });
-
 const app = express();
 const PORT = 3000;
 
@@ -18,8 +17,10 @@ app.post('/upload', upload.array('files', 100), (req, res, next) => {
     console.log(`${req.files.length} files loaded`);
     zipArray = req.files;
     // zipArray.sort(compare);
-    console.log(zipArray);
+    // console.log(zipArray);
     // console.log(getClearNumbersStr(zipArray[0]));
+
+    openZipArray();
 })
 
 app.get('/', (req, res) => {
@@ -30,15 +31,13 @@ app.listen(PORT, () => console.log(`The Server is running on port ${PORT}...`));
 
 async function openZipArray() {
 
-   
-    // const reader = new zip.ZipReader(zipArray[0]);
-    // const entries = await reader.getEntries();
-    // if(entries.length){
-    //     console.log(entries[0]);
-    // }
-}
+let jpegArray = await unzipper.Open.file(zipArray[0].path);
+jpegArray.files.pop();
+console.log(jpegArray.files);
 
-function compare(a, b) {
+} 
+
+function compare(a, b) { //переделать 
     if (getClearNumbersStr(a) < getClearNumbersStr(b)) {
         return -1;
     }
@@ -48,6 +47,6 @@ function compare(a, b) {
     return 0;
 }
 
-function getClearNumbersStr(a){
+function getClearNumbersStr(a) {
     return a.originalname.replace(/[^\d]/g, '0');
 }
