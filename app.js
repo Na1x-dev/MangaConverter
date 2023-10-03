cb1301212493960bb259a91a37117bbbbd008bc6
Na1x <na1x.1024@gmail.com>
2023-10-03 16:39:58 +0300

    feat: add file sort

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import * as fs from "node:fs";
 import unzipper from 'unzipper';
 import imgToPDF from 'image-to-pdf'
+import path from 'node:path';
 import { PDFDocument, StandardFonts } from 'pdf-lib';
 
 const upload = multer({ dest: 'uploads/' });
@@ -10,7 +11,7 @@ const app = express();
 const PORT = 3000;
 
 let pdfDoc = 0;
-
+let pdfName = '';
 let zipArray = [];
 
 app.set('view engine', 'ejs');
@@ -20,17 +21,29 @@ app.use(express.static('public'));
 app.post('/upload', upload.array('files', 100), (req, res, next) => {
     console.log(`${req.files.length} files loaded`);
     zipArray = req.files;
+
+    zipArray.sort(compare);
     createPDFfile();
     openZipArray();
+    setInterval(() => {
+        clearUploads();
+        // res.download('output/' + pdfName + '.pdf');
+    }, 1000)
+    // clearUploads();
+
+
 })
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+
 app.listen(PORT, () => console.log(`The Server is running on port ${PORT}...`));
 
 async function openZipArray() {
+    findMangaName();
+
     for (let zip of zipArray) {
         let jpegArray = await unzipper.Open.file(zip.path);
         jpegArray.files.pop();
@@ -40,8 +53,27 @@ async function openZipArray() {
         }
         await addImgToPDF(bufferArray);
     }
+
     const pdfBytes = await pdfDoc.save();
-    await fs.promises.writeFile('output.pdf', pdfBytes);
+    await fs.promises.writeFile('output/' + pdfName.trim() + '.pdf', pdfBytes);
+}
+
+function findMangaName() {
+    for (let match of zipArray[0].originalname.match(/[a-zA-Z]+ /g))
+        pdfName += match;
+    pdfName = pdfName.trimEnd();
+}
+
+function clearUploads() {
+    const directory = 'uploads';
+    fs.readdir(directory, (err, files) => {
+        if (err) console.log(err);
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) console.log(err);
+            });
+        }
+    });
 }
 
 async function createPDFfile() {
@@ -73,5 +105,6 @@ function compare(a, b) { //переделать
 }
 
 function getClearNumbersStr(a) {
-    return a.originalname.replace(/[^\d]/g, '0');
+    let chaptersNumbers = a.originalname.match(/ \d+ /g);
+    return parseFloat(chaptersNumbers[chaptersNumbers.length - 1]);
 }
