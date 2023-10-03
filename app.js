b6bdd814882681710f7cd0337dad9e0bc8753832
Na1x <na1x.1024@gmail.com>
2023-10-03 14:29:31 +0300

    feat: can convert from jpg to pdf

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ import express from 'express';
 import multer from 'multer';
 import * as fs from "node:fs";
 import unzipper from 'unzipper';
+import imgToPDF from 'image-to-pdf'
+import { PDFDocument, StandardFonts } from 'pdf-lib';
 
 const upload = multer({ dest: 'uploads/' });
 const app = express();
 const PORT = 3000;
+
+let pdfDoc = 0;
 
 let zipArray = [];
 
@@ -16,10 +20,7 @@ app.use(express.static('public'));
 app.post('/upload', upload.array('files', 100), (req, res, next) => {
     console.log(`${req.files.length} files loaded`);
     zipArray = req.files;
-    // zipArray.sort(compare);
-    // console.log(zipArray);
-    // console.log(getClearNumbersStr(zipArray[0]));
-
+    createPDFfile();
     openZipArray();
 })
 
@@ -30,12 +31,36 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => console.log(`The Server is running on port ${PORT}...`));
 
 async function openZipArray() {
+    for (let zip of zipArray) {
+        let jpegArray = await unzipper.Open.file(zip.path);
+        jpegArray.files.pop();
+        let bufferArray = [];
+        for (let jpegObj of jpegArray.files) {
+            bufferArray.push(await jpegObj.buffer());
+        }
+        await addImgToPDF(bufferArray);
+    }
+    const pdfBytes = await pdfDoc.save();
+    await fs.promises.writeFile('output.pdf', pdfBytes);
+}
 
-let jpegArray = await unzipper.Open.file(zipArray[0].path);
-jpegArray.files.pop();
-console.log(jpegArray.files);
+async function createPDFfile() {
+    pdfDoc = await PDFDocument.create();
+}
 
-} 
+async function addImgToPDF(images) {
+    for (let imageBuffer of images) {
+        const image = await pdfDoc.embedJpg(imageBuffer);
+        const imagePage = pdfDoc.addPage();
+        const { width, height } = image.scaleToFit(imagePage.getWidth(), imagePage.getHeight());
+        imagePage.drawImage(image, {
+            x: 0,
+            y: 0,
+            width,
+            height,
+        });
+    }
+}
 
 function compare(a, b) { //переделать 
     if (getClearNumbersStr(a) < getClearNumbersStr(b)) {
